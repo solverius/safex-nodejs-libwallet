d99f29e7564e0c0c6a00f903841f77d319d1ecb4
Daniel Dabek <xordaniel@gmail.com>
2020-02-23 23:27:19 -0500

    Update addon.cc

--- src/addon.cc
+++ src/addon.cc
@@ -48,7 +48,7 @@ NAN_METHOD(SetupLog) {
         return;
     }
 
-    Nan::Utf8String filename(info[1]->ToString(context).ToLocalChecked());
+    Nan::Utf8String filename(info[1]->ToString().ToLocalChecked());
     mlog_configure(*filename, false);
     mlog_set_log(configureString);
 }
