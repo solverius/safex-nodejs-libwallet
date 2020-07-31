ce436e79e57d64a39bbb4a3f60feb771431df70f
Igor Grkavac <grkavac.igor@gmail.com>
2020-07-31 15:37:04 +0200

    Change example to newer API

--- example/test-create-advanced-tx.js
+++ example/test-create-advanced-tx.js
@@ -15,25 +15,13 @@ var args = {
 	'path': wallet_path,
 	'password': '123',
 	'network': "testnet",
-	'daemonAddress': '127.0.0.1:29393',
-	'restoreHeight': 195000,
-	'addressString':  'SFXtzS7mTDsA9Sgp8D9EfDJq1bqQtc6TVckF8QP94QH6XkUKs8WZq7D6fcR6DtvoCdUch3y5FdxT1NH3gnE2symR7mvo5aYXTkp',
-	'viewKeyString':  '15593dfcbd182ce6640700593ae2a9fa722ff1edd2acaba14be13b2c16916c0a',
-	'spendKeyString':  '1d60513005ed051dec31aa8d22dbd8f2c1bd6171d504aa485a487940ef49a10b'
+	'daemonAddress': '127.0.0.1:37001',
+	'restoreHeight': 1,
+	'addressString':  'SFXtzV7tt2KZqvpCWVWauC5Qf16o3dAwLKNd9hCNzoB21ELLNfFjAMjXRhsR3ohT1AeW8j3jL4gfRahR86x6aoiU5hm5ZJj7BSc',
+	'viewKeyString':  '1ddc70c705ca023ccb08cf8d912f58d815b8e154a201902c0fc67cde52b61909',
+	'spendKeyString':  'c55a2fa96b04b8f019afeaca883fdfd1e7ee775486eec32648579e9c0fab950c'
 }
 
-if (!safex.walletExists(wallet_path)) {
-	console.log("wallet doesn't exist. creating new one: " + wallet_path);
-	if(args.mnemonic)
-		promise = safex.recoveryWallet(args);
-	else if(args.addressString)
-		promise = safex.createWalletFromKeys(args);
-	else
-		promise = safex.createWallet(args);
-} else {
-	console.log("wallet already exists. opening: " + wallet_path);
-	promise = safex.openWallet(args);
-}
 
 const nextTick = () => {
     if (wallet) {
@@ -52,9 +40,15 @@ const nextTick = () => {
     setTimeout(nextTick, 10000);
 }
 var lastHeight = 0;
-promise
-	.then((w) => {
-		console.log("address: " + w.address());
+
+	console.log("wallet doesn't exist. creating new one: " + wallet_path);
+	safex.createWalletFromKeys(args, AsyncWorkerCompletion);
+
+	function AsyncWorkerCompletion (err, w) {
+    if (err) {
+        console.log("Wallet returned an error: ", err);
+    } else {
+        console.log("address: " + w.address());
 		console.log("seed: " + w.seed());
 
 		wallet = w;
@@ -76,9 +70,12 @@ promise
 		wallet.on('refreshed', function () {
 			console.log("wallet refreshed");
 
-			wallet.store()
-				.then(() => {console.log("wallet stored")})
-				.catch((e) => {console.log("unable to store wallet: " + e)})
+			wallet.store( function(err, res) {
+					if(err)
+						console.log("Error storing wallet");
+					else
+						console.log("wallet stored");
+				})
 
 			if (!sent) {
 				sent = true;
@@ -87,8 +84,9 @@ promise
 
 				wallet.createAdvancedTransaction({
 					// * Create Safex account
-						// 'tx_type': '6',
-						// 'safex_username': 'saved',
+						'tx_type': '6',
+						'safex_username': 'saved',
+						'mixin': '1'
 					// * Edit Safex account
 						// 	'tx_type': '7',
 						// 	'safex_username': 'saved',
@@ -145,24 +143,28 @@ promise
 						// 'safex_offer_id': 'd00ad413b3789e9d4159568f354a96354e60441ff456e05d53d727f0e42cb9f3',
 						// 'safex_purchase_quantity': 2, // testnet
 					// * Feedback
-					'tx_type': '10',
-					'address': 'SFXtzS7mTDsA9Sgp8D9EfDJq1bqQtc6TVckF8QP94QH6XkUKs8WZq7D6fcR6DtvoCdUch3y5FdxT1NH3gnE2symR7mvo5aYXTkp', // testnet
-					'amount': '1',
-					'safex_offer_id': 'd00ad413b3789e9d4159568f354a96354e60441ff456e05d53d727f0e42cb9f3',
-					'safex_feedback_stars_given': 2,
-					'safex_feedback_comment': 'Not great, not terrible...',
-				}).then((tx) => {
-					console.log("transaction created: " + tx.transactionsIds());
-					
-					tx.commit().then(() => {
-						console.log("transaction commited successfully");
-					}).catch((e) => {
-						console.log("error on commiting transaction: " + e);
-                    });
-                    return wallet;
-				}).catch((e) => {
-					sent = false;
-					console.log("couldn't create transaction: " + e);
+						// 'tx_type': '10',
+						// 'address': 'SFXtzS7mTDsA9Sgp8D9EfDJq1bqQtc6TVckF8QP94QH6XkUKs8WZq7D6fcR6DtvoCdUch3y5FdxT1NH3gnE2symR7mvo5aYXTkp', // testnet
+						// 'amount': '1',
+						// 'safex_offer_id': 'd00ad413b3789e9d4159568f354a96354e60441ff456e05d53d727f0e42cb9f3',
+						// 'safex_feedback_stars_given': 2,
+						// 'safex_feedback_comment': 'Not great, not terrible...',
+				}, function(err, tx){
+
+						if(err)
+							console.log("transaction error: " + err);
+						else {
+							console.log("transaction created: " + tx.transactionsIds());
+
+							tx.commit(function(err, res){
+								if(err)
+									console.log("transaction commit error: " + err);
+								else
+									console.log("transaction commited successfully");
+
+
+							});
+						}
 				})
 			}
 		});
@@ -170,6 +172,10 @@ promise
 		wallet.on('updated', function () {
 			console.log("updated");
 		});
+
+		wallet.on('newBlock', function(height){
+			console.log("new height is: " + height);
+		})
 
 		wallet.on('unconfirmedMoneyReceived', function(tx, amount) {
 			console.log("unconfirmed money received. tx: " + tx + ", amount: " + amount);
@@ -195,8 +201,6 @@ promise
             console.log("tokens spent. tx: " + tx + ", token amount: " + token_amount);
         });
 
-		nextTick();
-	})
-	.catch((e) => {
-		console.log("no luck tonight: " + e);
-	});
+        nextTick();
+    }
+};
