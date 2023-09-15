f94bbb9966fa3412a1a9dc460adda9103f267dc2
safexninja <83340537+safexninja@users.noreply.github.com>
2023-09-15 13:15:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 if [ $build_type = "windows" ]
 then
+	mkdir -p $(pwd)/build;
 	make deps-win && cmake-js build --CDARCH="x86-64" --CDBUILD_WIN=ON --CDBUILD_64=ON --arch=x86_64 --CDCMAKE_TOOLCHAIN_FILE=./safexcore/contrib/depends/x86_64-w64-mingw32/share/toolchain.cmake -m;
-	mkdir -p lib/binding;
+	mkdir -p $(pwd)/lib/binding;
 	cp build/safex.node lib/binding/;
 	cp deps/libwallet_api.dll lib/binding/
 	cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll lib/binding/
 	cp /usr/lib/gcc/x86_64-w64-mingw32/*-posix/libgcc_s_seh-1.dll lib/binding/
 	cp /usr/lib/gcc/x86_64-w64-mingw32/*-posix/libstdc++-6.dll lib/binding/
 else
-	mkdir -p mkdir -p $(pwd)/build;
+	mkdir -p $(pwd)/build;
 	make deps && cmake-js build --CDARCH='x86-64' --CDBUILD_WIN=OFF --CDBUILD_64=ON --arch=x86_64 -m;
-	mkdir -p mkdir -p $(pwd)/buildlib/binding;
+	mkdir -p mkdir -p $(pwd)/lib/binding;
 	cp build/Release/safex.node lib/binding/;
 fi
